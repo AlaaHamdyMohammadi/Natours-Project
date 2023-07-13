fca28c1e776833a4c8031943d037d07b8eaab76f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-13 19:10:36 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+//middleware
+app.use(express.json());
+
 // app.get('/', (req, res)=>{
 //     // res.status(200).send('Hello from the server side')
 //     res.status(200).json({message: 'Hello from the server side', app: 'Natours'})
@@ -22,7 +25,23 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
-    })
+    });
+});
+
+app.post('/api/v1/tours', (req,res) =>{
+    console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data:{
+                tours, newTour,
+            }
+        })
+    });
+    //res.send('done');
 })
 
 const port = 8000;
