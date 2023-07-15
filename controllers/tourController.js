374c3f1a86a66be42d0b9f43464da402db23292b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-15 19:25:38 +0200

    UpdateTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -63,15 +63,27 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
+exports.updateTour = async (req, res) => {
+  try{
+    const tour =  await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
     status: 'success',
-    /*
     data: {
-      tour: '<Updated tour .. >',
+      tour,
     },
-    */
+    
   });
+  }.catch(err){
+    res.status(400).json({
+      status: 'Faild',
+      message: 'Invalid data sent',
+    })
+  }
+  
 };
 
 exports.deleteTour = (req, res) => {
