ed6044170e2f0e9d79efd03d1287878fae0fc8af
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-18 01:10:33 +0200

    CustomValidate

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
+
 
 const tourSchema = new mongoose.Schema(
   {
@@ -11,6 +13,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equal then 40 characters'],
+      //validator: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -43,7 +46,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priseDiscount: Number,
+    priseDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val){
+          return val < this.price;
+        },
+        message: 'Error number',
+      } 
+    },
     summary: {
       type: String,
       trim: true,
