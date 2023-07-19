540fa3fc8d37f40f59073032e0a2fca39fde531d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-20 01:37:02 +0200

    ImplementGlobalError

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Faild',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  })
-})
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -30,10 +24,35 @@ app.use((req, res, next) => {
 });
 
 
-
 // 3) routes
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter); 
+
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Faild',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  //Any parameter in next method = error
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'Error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+});
+
+
 
 module.exports = app;
