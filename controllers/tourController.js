d15329e1c718805b76a5ed74017a3895a8a7232c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-20 17:20:38 +0200

    CatchErrorAsyncFunc

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,6 +12,7 @@ exports.aliasTopTours = (req, res, next) => {
 
 
 exports.getAllTours = catchAsync(async(req, res, next) => {
+    //console.log(req.query);
     console.log(req.query);
 
     //Execute query
