229ab84cc45f3e079f4f24aff495cb3cc159bb0e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-30 22:43:39 +0200

    guidesEmbedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -28,9 +28,9 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: ['easy', 'meduim', 'difficult'],
-        message: 'Error value',
-      }
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingAverage: {
       type: Number,
@@ -49,11 +49,11 @@ const tourSchema = new mongoose.Schema(
     priseDiscount: {
       type: Number,
       validate: {
-        validator: function(val){
+        validator: function (val) {
           return val < this.price;
         },
         message: 'Error number',
-      } 
+      },
     },
     summary: {
       type: String,
@@ -79,6 +79,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -96,6 +121,12 @@ tourSchema.pre('save', function(next){
   this.slug = slugify(this.name, {lower: true});
   next();
 });
+
+tourSchema.pre('save',async function(next){
+  const guidesPromises = this.guides.map(async id => await User.findById(id))
+  this.guides = await Promise.all(guidesPromises);
+  next();
+})
 
 //document param that save in db
 // tourSchema.post('save', function(doc, next){
