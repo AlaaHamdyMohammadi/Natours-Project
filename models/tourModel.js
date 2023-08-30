ca74a9b4f970c6dce1b38f15f2a0a25c89a1a86c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-08-30 22:56:14 +0200

    GuidesRefference

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,7 +103,11 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    //guides: Array,//Embedding
+    guides: [
+      {type: mongoose.Schema.ObjectId,
+      ref: 'User'}
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -122,11 +126,13 @@ tourSchema.pre('save', function(next){
   next();
 });
 
-tourSchema.pre('save',async function(next){
-  const guidesPromises = this.guides.map(async id => await User.findById(id))
-  this.guides = await Promise.all(guidesPromises);
-  next();
-})
+
+//Embedding connect between users and tours.
+// tourSchema.pre('save',async function(next){
+//   const guidesPromises = this.guides.map(async id => await User.findById(id))
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 //document param that save in db
 // tourSchema.post('save', function(doc, next){
